4334b013b3d33922cb61956eaa261d3cd1166b39
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-05 16:49:36 +0530

    Add Tests Passing Including Integration Test

--- src/main/java/com/hcl/springboottdd/SpringbootTddApplication.java
+++ src/main/java/com/hcl/springboottdd/SpringbootTddApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @EnableCaching
 public class SpringbootTddApplication {
 
--- src/test/java/com/hcl/springboottdd/web/CarControllerTest.java
+++ src/test/java/com/hcl/springboottdd/web/CarControllerTest.java
@@ -6,9 +6,11 @@ import com.hcl.springboottdd.service.CarService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -18,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(CarController.class)
 public class CarControllerTest {
 
     @Autowired
