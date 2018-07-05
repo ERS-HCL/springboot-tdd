33ca2bc7d7513b0eb00d10f33f94fb9f3900fabf
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-05 16:34:18 +0530

    Add Tests Passing Including Integration Test

--- src/main/java/com/hcl/springboottdd/service/CarService.java
+++ src/main/java/com/hcl/springboottdd/service/CarService.java
@@ -3,6 +3,7 @@ package com.hcl.springboottdd.service;
 import com.hcl.springboottdd.domain.Car;
 import com.hcl.springboottdd.domain.CarRepository;
 import com.hcl.springboottdd.exception.CarNotFoundException;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class CarService {
         this.carRepository = carRepository;
     }
 
+    @Cacheable("cars")
     public Car getCarDetails(String name) {
         Car car = carRepository.findByName(name);
         if (car == null) {
