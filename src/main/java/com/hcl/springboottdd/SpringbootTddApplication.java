c80a6225e357633ff16e8079beddb6b5f51739c8
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-05 16:22:08 +0530

    Added Repository Tests

--- src/main/java/com/hcl/springboottdd/SpringbootTddApplication.java
+++ src/main/java/com/hcl/springboottdd/SpringbootTddApplication.java
@@ -2,10 +2,12 @@ package com.hcl.springboottdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableCaching
 public class SpringbootTddApplication {
 
 	public static void main(String[] args) {
